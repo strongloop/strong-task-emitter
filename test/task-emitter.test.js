dca09eae1c4e396107647f10688442abe60553d5
Ritchie Martori <skawful@gmail.com>
2013-04-29 08:52:15 -0700

    Add test

--- test/task-emitter.test.js
+++ test/task-emitter.test.js
@@ -1,24 +1,35 @@
 var TaskEmitter = require('../');
 
 describe('TaskEmitter', function(){
-  var taskEmitter;
+  var te;
   
   beforeEach(function(){
-    taskEmitter = new TaskEmitter;
+    te = new TaskEmitter;
   });
   
-  describe('.myMethod', function(){
-    // example sync test
-    it('should <description of behavior>', function() {
-      taskEmitter.myMethod();
-    });
-    
-    // example async test
-    it('should <description of behavior>', function(done) {
-      setTimeout(function () {
-        taskEmitter.myMethod();
-        done();
-      }, 0);
+  describe('.task(scope, functionName, args...)', function(){
+    it('should call the function with the provided args', function(done) {
+      var called = 0;
+      
+      var math = {
+        sum: function (a, b, c, fn) {
+          called++;
+          
+          setTimeout(function () {
+            fn(null, a + b + c)
+          }, 1);
+        }
+      }
+      
+      te
+        .task(math, 'sum', 2, 2, 2)
+        .task(math, 'sum', 2, 2, 2)
+        .task(math, 'sum', 2, 2, 2)
+        .on('error', done)
+        .on('done', function () {
+          assert.equal(called, 3, 'should call the sum function 3 times');
+          done();
+        });
     });
   });
 });
