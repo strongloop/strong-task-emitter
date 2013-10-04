bf13307e9914f4f08c0de09a84cc2e61a10b566c
Sam Roberts <sam@strongloop.com>
2013-10-04 10:58:42 -0700

    Demo: a feature not merged to develop

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ module.exports = require('./lib/task-emitter');
 module.exports.TaskEmitter = require('./lib/task-emitter');
 module.exports.Task = require('./lib/task');
 module.exports.developFeature = 'feature merged to develop, but not released to npmjs.org';
+module.exports.unmergedFeature = 'feature not merged to develop';
