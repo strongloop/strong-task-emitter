0c9a56fd6ce02c5af4f41e8f91e8680d3dd25f6d
Sam Roberts <sam@strongloop.com>
2013-10-04 10:29:18 -0700

    Demo: develop feature, not on npmjs.org

--- index.js
+++ index.js
@@ -5,3 +5,4 @@
 module.exports = require('./lib/task-emitter');
 module.exports.TaskEmitter = require('./lib/task-emitter');
 module.exports.Task = require('./lib/task');
+module.exports.developFeature = 'feature merged to develop, but not released to npmjs.org';
